99843fe28bc2fb81a9a4ee24cddb3908d6901d3c
David Amar <david.amar@moodys.com>
2019-02-18 10:04:20 +0100

    Split game engine and game scenario 2 files, game.js and places.js Game logic is in game.js Places details (coords and hints) are in places.js

--- game.js
+++ game.js
@@ -1,19 +1,3 @@
-var places = [
-  [45.181892, 5.722390, 'Vers la caserne de Bonne'],
-  [45.184168, 5.722311, 'En allant vers l\'école'],
-  [45.185029, 5.723160, 'Piscine...'],
-  [45.185624, 5.722760, 'De l\'autre côté de la rue'],
-  [45.186533, 5.723624, 'En avançant dans la ruelle'],
-  [45.186532, 5.722804, 'A gauche'],
-  [45.187176, 5.722497, 'On peut demander de l\'aide aux gens'],
-  [45.188017, 5.721318, 'En revenant sur la grande avenue'],
-  [45.187903, 5.719111, 'Vers le tramway'],
-  [45.183371, 5.718063, 'Direction la Makolette'],
-  [45.182770, 5.717695, 'C\'est très grand !'],
-  [45.182149, 5.717807, 'Derrière la Makolette'],
-  [45.180376, 5.719218, 'Par dessus le pont'],
-  [45.180516, 5.722224, 'Retour à la case départ ???']
-];
 var defaultRange = 30;
 var invalidPlaceErrorMessage = '<b>Non, ce n\'est pas ici...</b>';
 var finalMessage = 'Je crois qu\'on est arrivé...';
