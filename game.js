ff945483739032a067d764f56d4b18e8216f992a
David Amar <david.amar@moodys.com>
2019-02-18 10:57:00 +0100

    Game designer feature Add new game designer to record places - Start generator.html - Go to target location, press Add - Add hint - Loop until game desgin is completed - Replace places.js by generated code
    An automatically generated URL is placed at the en of places.js to
    resume game design if needed.

--- game.js
+++ game.js
@@ -3,12 +3,6 @@ var invalidPlaceErrorMessage = '<b>Non, ce n\'est pas ici...</b>';
 var finalMessage = 'Je crois qu\'on est arrivé...';
 
 var nbPlaces = places.length;
-var timeout = null;
-
-function getParam(paramName)
-{
-  return (window.location.href.split('?')[1].split('&').map(x=>x.split("=")).filter(x=>x[0]==paramName)[0]||[null,null])[1];
-}
 
 function getId()
 {
@@ -34,8 +28,7 @@ window.onload = function()
   id = getId();
   if(id == nbPlaces)
   {
-    showMessage(finalMessage);
-    window.clearTimeout(timeout);
+    showMessage(finalMessage, true);
     var btns = document.getElementsByTagName('input');
     for(var i=0; i<btns.length; i++)
       btns[i].disabled = 'disabled';
@@ -55,57 +48,14 @@ window.onload = function()
   }
 }
 
-function degreesToRadians(degrees) {
-  return degrees * Math.PI / 180;
-}
-
-function distance(lat1, lon1, lat2, lon2) {
-  var earthRadiusM = 6371000;
-
-  var dLat = degreesToRadians(lat2-lat1);
-  var dLon = degreesToRadians(lon2-lon1);
-
-  lat1 = degreesToRadians(lat1);
-  lat2 = degreesToRadians(lat2);
-
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  return earthRadiusM * c;
-}
-
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-
-function getLocationAndThen(doThis)
-{
-  navigator.geolocation.getCurrentPosition(
-    pos => doThis([pos.coords.latitude, pos.coords.longitude, pos.coords.accuracy]),
-    err => showMessage(`ERREUR (${err.code}): ${err.message}`),
-    options);
-}
-
-function isCloseTo(lat, lon, range, success, failure)
-{
-  getLocationAndThen(x=>
-  {
-    var effectiveRange = x[2];
-    if(effectiveRange < range) effectiveRange = range;
-    if(debug) effectiveRange = 1000000;
-    if(distance(x[0],x[1],lat,lon) < effectiveRange)
-      success();
-    else
-      failure();
-  });
-}
-
 function testFind()
 {
-  isCloseTo(targetLat, targetLong, defaultRange,
-    ()=>window.location.href = window.location.href.replace(/place=[0-9]+/,'place='+(++id)),
+  var successFunction = ()=>window.location.href = window.location.href.replace(/place=[0-9]+/,'place='+(++id));
+  if (debug)
+ 	  successFunction();
+  else
+	  isCloseTo(targetLat, targetLong, defaultRange,
+    successFunction,
     ()=>showMessage(invalidPlaceErrorMessage));
 }
 
@@ -117,13 +67,4 @@ function showDistance()
 function showHint()
 {
   showMessage(hint);
-}
-
-function showMessage(msg)
-{
-  window.clearTimeout(timeout);
-  var elem = document.getElementById('message');
-  elem.innerHTML = msg;
-  elem.style.display = 'inline';
-  timeout = window.setTimeout(function(){elem.style.display = 'none';}, 2000);
 }
